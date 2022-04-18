e7a26ae71a7e40eab7461416c50610d0216a7277
Lucas Vieira <lucas.engen.cc@gmail.com>
2022-04-18 00:28:38 -0300

    fix: :bug: fixes cors settings
    Signed-off-by: Lucas Vieira <lucas.engen.cc@gmail.com>

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import { logError, logInfo } from "./log";
 
 export function startHttp() {
@@ -16,6 +17,9 @@ export function startHttp() {
     return res.status(200).json({ alive: true, pid: `${process.pid}` });
   });
 
+  app.use(express.json());
+  app.use(cors());
+  app.use(routes);
   app.listen(PORT);
   logInfo("started HTTP server");
 }
